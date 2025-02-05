cbc87420f1e0347f911fe21af68621563427214e
Maia Rowe <mrowe7000@icloud.com>
2025-02-05 06:54:52 +0000

    app and erb updates

--- app.rb
+++ app.rb
@@ -1,9 +1,63 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
 
-get("/") do
+
+
+get "/" do
   "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
+  <h1>ROCK, PAPER, SCISSORS</h1>
+  <ul>
+    <li><a href='/rock'>Play Rock</a></li>
+    <li><a href='/paper'>Play Paper</a></li>
+    <li><a href='/scissors'>Play Scissors</a></li>
+  </ul>
   "
+end
+
+
+
+
+
+get ("/rock") do
+  moves = ["rock","paper","scissors"]
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  erb(:zebra)
+end
+
+get ("/paper") do
+  moves = ["rock","paper","scissors"]
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "won"
+  elsif @comp_move == "paper"
+    @outcome = "tied"
+  else
+    @outcome = "lost"
+  end
+  erb(:paper)
+end
+
+get ("/scissors") do
+  moves = ["rock","paper","scissors"]
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "lost"
+  elsif @comp_move == "paper"
+    @outcome = "won"
+  else
+    @outcome = "lost"
+  end
+  erb(:paper)
 end
